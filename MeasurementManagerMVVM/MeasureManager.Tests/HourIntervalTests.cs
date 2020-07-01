acbaae05ae73d8aa5e550dc9d30e8b817e07e18f
lazycorkscrew <54800143+lazycorkscrew@users.noreply.github.com>
2020-07-02 03:55:46 +0400

    Попытка написать первый тест

---
+++ MeasurementManagerMVVM/MeasureManager.Tests/HourIntervalTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using MeasurementManagerMVVM.Models;
+using ut =Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using NUnit.Framework.Internal.Commands;
+
+namespace MeasureManager.Tests
+{
+    [TestFixture]
+    public class HourIntervalTests
+    {
+        [ut.ExpectedException(typeof(ArgumentException))]
+        public void TestMethod(int a, int b)
+        {
+            HourInterval hi = new HourInterval(a, b);
+        }
+
+        [Test, Category("pass")]
+        [TestCase(0, 23)]
+        [TestCase(2, 2)]
+        [TestCase(2, 1)]
+        [TestCase(2, 6)]
+        [TestCase(23, 23)]
+        public void CreateCorrectIntervals(int a, int b)
+        {
+
+            //Assert.DoesNotThrow(TestMethod(a, b));
+        }
+        
+    }
+}
---
+++ MeasurementManagerMVVM/MeasureManager.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MeasureManager.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("MeasureManager.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa3ebdd6-5c52-476d-bb0a-d8fd8fdef571")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
